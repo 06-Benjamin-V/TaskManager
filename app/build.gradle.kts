6d210e3fb6afaf6df441b228a9e27d4b881851a4
Christian21321 <164412626+Christian21321@users.noreply.github.com>
2024-07-06 16:32:43 -0400

    intento conexion API google calendar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.taskmanager"
     compileSdk = 34
 
+    packagingOptions {
+        exclude ("META-INF/DEPENDENCIES")
+    }
+
     defaultConfig {
         applicationId = "com.example.taskmanager"
         minSdk = 30
@@ -24,13 +28,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -40,6 +43,12 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.airbnb.android:lottie:4.1.0")
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
-    implementation ("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("com.google.firebase:firebase-firestore-ktx")
+
+    // Dependencias de la API de Google Calendar
+    implementation ("com.google.api-client:google-api-client:2.0.0")
+    implementation ("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+    implementation ("com.google.apis:google-api-services-calendar:v3-rev20220715-2.0.0")
 }
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "TaskManager"
 include(":app")
+
+
